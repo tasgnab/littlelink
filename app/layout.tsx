fec27134ec297f819e4deb32e38dfc6af5c435e6
thopo <thopo.martha@gmail.com>
2025-12-06 16:20:22 +1100

    feat: add SpeedInsights component and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { clientConfig } from "@/lib/config";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: clientConfig.title,
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
